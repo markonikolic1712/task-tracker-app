46634f5d54153c982af48e68874f474753c9c83b
marknikolic <marko.nikolic@unicreditgroup.rs>
2025-07-23 14:51:27 +0200

    initial commit

---
+++ src/test/java/com/marko/task_tracker_app/TaskTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marko.task_tracker_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
